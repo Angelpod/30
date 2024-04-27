fd2529770d5683ef33f1c9ecb169d98a3ce8893b
Angelpod <149606603+Angelpod@users.noreply.github.com>
2024-04-27 15:09:21 +0300

    Comment to commit

---
+++ админка/routes/games.js
@@ -0,0 +1,12 @@
+
+const gamesRouter = require("express").Router(); 
+const { sendAllGames , deleteGame , addGameController} = require("../controllers/games");
+const { getAllGames } = require("../middlwares/games")
+
+
+
+gamesRouter.post("/games",getAllGames, addGameController);
+gamesRouter.get("/games",getAllGames, sendAllGames);
+gamesRouter.delete("/games/:id",getAllGames, deleteGame);
+
+module.exports = gamesRouter;
